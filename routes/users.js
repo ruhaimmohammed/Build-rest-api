c5cc28127ef6e18e3e3ad3f7086dc4719de1c3cb
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-02-19 02:42:17 +0530

    Delete operation with ID with params

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
     res.send(users);
@@ -16,5 +16,21 @@ router.post('/', (req, res) => {
 
     res.send(`User with name ${user.firstName} added to db`)
 });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id == id);
+
+    res.send(foundUser)
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted from th DB`)
+})
 
 export default router;
