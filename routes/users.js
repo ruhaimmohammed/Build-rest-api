0ee7bffa77be6f45b36bc8e037de596eccd189ff
Ruhaim Mohammed <ruhaimmohammed@gmail.com>
2022-02-19 02:21:48 +0530

    Created post req with id from uuid package

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
+const router = express.Router();
+
+const users = [];
+
+router.get('/', (req, res) => {
+    res.send(users);
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4()});
+
+    res.send(`User with name ${user.firstName} added to db`)
+});
+
+export default router;
